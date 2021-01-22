6e616e9718c6f3de50f9623d06dffa8e61947dff
melitus <asmelitus@gmail.com>
2021-01-22 15:22:07 +0100

    feat: add dotenv
    Dotenv is used load environment variables from a . env file into process.env

---
+++ src/config/index.ts
@@ -0,0 +1,27 @@
+import path from 'path';
+import * as dotenv from 'dotenv';
+
+const envPath = path.join(
+  process.cwd(),
+  `.env${!process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? '' : `.${process.env.NODE_ENV}`}`,
+);
+
+const loadenv = () =>
+  dotenv.config({
+    path: envPath,
+  });
+
+loadenv();
+
+export default {
+ appKey: {
+    port: process.env.APP_PORT || 8000,
+    env: process.env.NODE_ENV || 'production'
+  },
+  mongo: {
+    uri:
+      process.env.NODE_ENV === 'test'
+        ? process.env.MONGO_URI_TESTS
+        : process.env.MONGO_URI
+  }
+};
